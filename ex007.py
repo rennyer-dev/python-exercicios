0b749720d97e1edf0ecaf0eea1479f8fb9d4851e
josé rennyer <rennyerpro@gmail.com>
2026-02-02 17:28:26 -0300

    exercicios 02/02/2026

---
+++ ex007.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia as duas notas
+# de um aluno, calcule e mostre sua média.
+
+n1 = float(input('Informe a 1ª nota: '))
+n2 = float(input('Informe a 2ª nota: '))
+media = (n1 + n2) / 2
+
+print('A média do aluno é {:.1f}'.format(media))
